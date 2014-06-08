40fe29b6042f55cd0f2b5d2dd242ab649b7fa6e1
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2014-06-09 00:50:44 +0900

    add Common.java

---
+++ Common.java
@@ -0,0 +1,70 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Common {
+	MyScanner sc = new MyScanner();
+	Scanner sc2 = new Scanner(System.in);
+
+	void run() {
+		
+	}
+
+	public static void main(String[] args) {
+		new Common().run();
+	}
+
+	
+	void debug(Object... o) {
+		System.out.println(Arrays.deepToString(o));
+	}
+
+	void debug2(int[][] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.print(array[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	class MyScanner {
+		int nextInt() {
+			try {
+				int c = System.in.read();
+				while (c != '-' && (c < '0' || '9' < c))
+					c = System.in.read();
+				if (c == '-')
+					return -nextInt();
+				int res = 0;
+				do {
+					res *= 10;
+					res += c - '0';
+					c = System.in.read();
+				} while ('0' <= c && c <= '9');
+				return res;
+			} catch (Exception e) {
+				return -1;
+			}
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String next() {
+			try {
+				StringBuilder res = new StringBuilder("");
+				int c = System.in.read();
+				while (Character.isWhitespace(c))
+					c = System.in.read();
+				do {
+					res.append((char) c);
+				} while (!Character.isWhitespace(c = System.in.read()));
+				return res.toString();
+			} catch (Exception e) {
+				return null;
+			}
+		}
+	}
+}
+
