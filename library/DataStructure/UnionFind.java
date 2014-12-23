7d68069c89469787a2794c57ee937b953d7eb72a
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2014-12-23 23:10:20 +0900

    add UnionFind

---
+++ library/DataStructure/UnionFind.java
@@ -0,0 +1,39 @@
+public class UnionFind {
+	int[] par;
+	int[] rank;
+
+	void init(int n) {
+		for (int i = 0; i < n; i++) {
+			par[i] = i;
+			rank[i] = 0;
+		}
+	}
+
+	int find(int x) {
+		if (par[x] == x) {
+			return x;
+		} else {
+			return par[x] = find(par[x]);
+		}
+	}
+
+	void unite(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x == y) {
+			return;
+		}
+		if (rank[x] < rank[y]) {
+			par[x] = y;
+		} else {
+			par[y] = x;
+			if (rank[x] == rank[y]) {
+				rank[x]++;
+			}
+		}
+	}
+
+	boolean same(int x, int y) {
+		return find(x) == find(y);
+	}
+}
