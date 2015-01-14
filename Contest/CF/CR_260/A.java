51af9ce38eba11b6f9195fe957942030ec47f936
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2015-01-15 01:06:12 +0900

    add CR 260

---
+++ Contest/CF/CR_260/A.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A {
+	Scanner sc2 = new Scanner(System.in);
+
+	void run() {
+		int n = sc2.nextInt();
+		Info[] info = new Info[n];
+		for (int i = 0; i < n; i++) {
+			info[i] = new Info(sc2.nextInt(), sc2.nextInt());
+		}
+		Arrays.sort(info);
+		boolean judge = false;
+		for (int i = 0; i < n - 1; i++) {
+			if (info[i].quality > info[i + 1].quality) {
+				judge = true;
+			}
+		}
+		System.out.println(judge ? "Happy Alex" : "Poor Alex");
+	}
+
+	class Info implements Comparable<Info> {
+		int price;
+		int quality;
+
+		public Info(int price, int quality) {
+			super();
+			this.price = price;
+			this.quality = quality;
+		}
+
+		@Override
+		public int compareTo(Info arg0) {
+			return this.price - arg0.price;
+		}
+	}
+
+	public static void main(String[] args) {
+		new A().run();
+	}
+}
