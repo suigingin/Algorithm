10176f4e459f05bc435c5a67838b81f6a2815db3
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2015-02-08 14:45:42 +0900

    make dir

---
+++ Contest/CF/2014/CR_260/B1.java
@@ -0,0 +1,51 @@
+import java.math.BigInteger;
+
+public class B1 {
+	MyScanner sc = new MyScanner();
+
+	void run() {
+		BigInteger in = new BigInteger(sc.next());
+		System.out.println(in.mod(new BigInteger("4")).equals(in.ZERO) ? 4 : 0);
+	}
+
+	public static void main(String[] args) {
+		new B1().run();
+	}
+
+	class MyScanner {
+		int nextInt() {
+			try {
+				int c = System.in.read();
+				while (c != '-' && (c < '0' || '9' < c))
+					c = System.in.read();
+				if (c == '-')
+					return -nextInt();
+				int res = 0;
+				do {
+					res *= 10;
+					res += c - '0';
+					c = System.in.read();
+				} while ('0' <= c && c <= '9');
+				return res;
+			} catch (Exception e) {
+				return -1;
+			}
+		}
+
+		String next() {
+			try {
+				StringBuilder res = new StringBuilder("");
+				int c = System.in.read();
+				while (Character.isWhitespace(c))
+					c = System.in.read();
+				do {
+					res.append((char) c);
+				} while (!Character.isWhitespace(c = System.in.read()));
+				return res.toString();
+			} catch (Exception e) {
+				return null;
+			}
+		}
+	}
+}
+
