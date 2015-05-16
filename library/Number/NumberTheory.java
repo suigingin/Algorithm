f4edc4838fdaf0bb66d20d3c3c41b5d57e56f13d
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2015-05-17 02:52:24 +0900

    add pascal

--- library/Number/NumberTheory.java
+++ library/Number/NumberTheory.java
@@ -90,6 +90,17 @@ public class NumberTheory {
 		return res;
 	}
 
+	void pascal() {
+		int[][] com = new int[1001][1001];
+		com[0][0] = 1;
+		for (int i = 1; i <= 1000; i++) {
+			for (int j = 0; j < 1000; j++) {
+				if (j == 0 || j == i) com[i][j] = 1;
+				else com[i][j] = (com[i - 1][j - 1] + com[i - 1][j]) % MOD;
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 	}
 }
