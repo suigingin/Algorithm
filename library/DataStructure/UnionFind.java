c8c192e50c23f95d742b5ca9a0aacc79ed825fbf
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2015-06-03 09:04:36 +0900

    update UnionFind

--- library/DataStructure/UnionFind.java
+++ library/DataStructure/UnionFind.java
@@ -1,31 +1,28 @@
 public class UnionFind {
+
 	int[] par;
 	int[] rank;
+	int groupCnt;
 
 	void init(int n) {
-		for (int i = 0; i < n; i++) {
-			par[i] = i;
-			rank[i] = 0;
-		}
+		groupCnt = n;
+		par = new int[n];
+		rank = new int[n];
+		Arrays.fill(par, -1);
 	}
 
-	int find(int x) {
-		if (par[x] == x) {
-			return x;
-		} else {
-			return par[x] = find(par[x]);
-		}
-	}
-
-	void unite(int x, int y) {
+	void union(int x, int y) {
 		x = find(x);
 		y = find(y);
 		if (x == y) {
 			return;
 		}
+		groupCnt--;
 		if (rank[x] < rank[y]) {
+			par[y] += par[x];
 			par[x] = y;
 		} else {
+			par[x] += par[y];
 			par[y] = x;
 			if (rank[x] == rank[y]) {
 				rank[x]++;
@@ -33,7 +30,23 @@ public class UnionFind {
 		}
 	}
 
+	int find(int x) {
+		if (par[x] < 0) {
+			return x;
+		} else {
+			return par[x] = find(par[x]);
+		}
+	}
+
 	boolean same(int x, int y) {
 		return find(x) == find(y);
+	}
+
+	int getGroupSize(int x) {
+		return -par[find(x)];
+	}
+
+	int getGroupCnt() {
+		return groupCnt;
 	}
 }
