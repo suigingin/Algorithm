3b5cdee6e76c778455725fbb5e6228362b61660e
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2016-02-01 09:17:17 +0900

    update CR_264(C)

--- Contest/CF/2014/CR_264/C.java
+++ Contest/CF/2014/CR_264/C.java
@@ -3,55 +3,44 @@ public class C {
 
 	void run() {
 		int n = sc.nextInt();
-		int[][] f = sc.nextInt2dArray(n, n);
-		long[] left = new long[n * 2 - 1];
-		long[] right = new long[n * 2 - 1];
-		for (int i = 0; i < n * 2 - 1; i++) {
-			int hl = i < n ? i : n - 1;
-			int wl = i < n ? 0 : i - n + 1;
-			while (inner(hl, wl, n, n)) {
-				left[i] += f[hl--][wl++];
-			}
-
-			int hr = i < n ? i : n - 1;
-			int wr = i < n ? n - 1 : n * 2 - i - 2;
-			while (inner(hr, wr, n, n)) {
-				right[i] += f[hr--][wr--];
-			}
-		}
-
-		int x1, y1, x2, y2;
-		x1 = y1 = x2 = y2 = 0;
-		long max1 = -1;
-		long max2 = -1;
+		int[][] f = new int[n][n];
+		long[] upper = new long[n * 2 + 10];
+		long[] lower = new long[n * 2 + 10];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				long score = left[i + j] + right[n - j + i - 1] - f[i][j];
+				f[i][j] = sc.nextInt();
+				upper[i - j + n] += f[i][j];
+				lower[i + j] += f[i][j];
+			}
+		}
+		long even = -1;
+		long odd = -1;
+		int h1, w1, h2, w2;
+		h1 = w1 = h2 = w2 = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				long sum = upper[i - j + n] + lower[i + j] - f[i][j];
 				if ((i + j) % 2 == 0) {
-					if (score > max1) {
-						max1 = score;
-						x1 = i + 1;
-						y1 = j + 1;
+					if (sum > even) {
+						even = sum;
+						h1 = i + 1;
+						w1 = j + 1;
 					}
 				} else {
-					if (score > max2) {
-						max2 = score;
-						x2 = i + 1;
-						y2 = j + 1;
+					if (sum > odd) {
+						odd = sum;
+						h2 = i + 1;
+						w2 = j + 1;
 					}
 				}
 			}
 		}
-		System.out.println(max1 + max2);
-		System.out.println(x1 + " " + y1 + " " + x2 + " " + y2);
+		System.out.println(even + odd);
+		System.out.println(h1 + " " + w1 + " " + h2 + " " + w2);
 	}
 
 	public static void main(String[] args) {
 		new C().run();
-	}
-
-	boolean inner(int h, int w, int limH, int limW) {
-		return 0 <= h && h < limH && 0 <= w && w < limW;
 	}
 
 	class MyScanner {
@@ -72,22 +61,6 @@ public class C {
 			} catch (Exception e) {
 				return -1;
 			}
-		}
-
-		int[] nextIntArray(int n) {
-			int[] in = new int[n];
-			for (int i = 0; i < n; i++) {
-				in[i] = nextInt();
-			}
-			return in;
-		}
-
-		int[][] nextInt2dArray(int n, int m) {
-			int[][] in = new int[n][m];
-			for (int i = 0; i < n; i++) {
-				in[i] = nextIntArray(m);
-			}
-			return in;
 		}
 	}
 }
