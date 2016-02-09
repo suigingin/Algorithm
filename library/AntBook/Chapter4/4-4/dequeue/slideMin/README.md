2229b6a7ba55f768d0330a7348c6f951eb0c7080
Manabu Yokohashi <saigyoujiyyk@gmail.com>
2016-02-10 01:33:39 +0900

    add SlideMin

---
+++ library/AntBook/Chapter4/4-4/dequeue/slideMin/README.md
@@ -0,0 +1,6 @@
+## スライド最小値
+### 計算量
+O(n)
+
+### verify
+http://poj.org/problem?id=2823
---
+++ library/AntBook/Chapter4/4-4/dequeue/slideMin/SlideMinMax.java
@@ -0,0 +1,41 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class SlideMinMax {
+
+	int[] getSlideMin(int[] a, int n, int k) {
+		int[] b = new int[n - k + 1];
+		Deque<Integer> deq = new LinkedList<Integer>();
+		for (int i = 0; i < n; i++) {
+			if (deq.isEmpty()) {
+				deq.add(i);
+			} else {
+				while (!deq.isEmpty() && a[deq.peekLast()] >= a[i]) deq.pollLast();
+				deq.addLast(i);
+			}
+			if (i - k + 1 >= 0) {
+				b[i - k + 1] = a[deq.peekFirst()];
+				if (deq.peekFirst() == i - k + 1) deq.pollFirst();
+			}
+		}
+		return b;
+	}
+
+	int[] getSlideMax(int[] a, int n, int k) {
+		int[] b = new int[n - k + 1];
+		Deque<Integer> deq = new LinkedList<Integer>();
+		for (int i = 0; i < n; i++) {
+			if (deq.isEmpty()) {
+				deq.add(i);
+			} else {
+				while (!deq.isEmpty() && a[deq.peekLast()] <= a[i]) deq.pollLast();
+				deq.addLast(i);
+			}
+			if (i - k + 1 >= 0) {
+				b[i - k + 1] = a[deq.peekFirst()];
+				if (deq.peekFirst() == i - k + 1) deq.pollFirst();
+			}
+		}
+		return b;
+	}
+}
